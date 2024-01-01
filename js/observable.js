8f8ad152f3d95d3b5884a2fdd84656fda02bb55b
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-01-01 07:46:30 -0800

    Add files via upload

--- js/observable.js
+++ js/observable.js
@@ -1,11 +1,15 @@
 
+
+
+
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
-import define from "https://api.observablehq.com/d/607d09907224a21d@1464.js?v=4";
+import define from "https://api.observablehq.com/d/607d09907224a21d.js?v=4";
 new Runtime().module(define, name => {
-  if (name === "viewof gradientEffect") return new Inspector(document.querySelector("#observablehq-viewof-gradientEffect-8955fadf"));
-  if (name === "viewof filterEffect") return new Inspector(document.querySelector("#observablehq-viewof-filterEffect-8955fadf"));
-  if (name === "viewof colorBlendEffect") return new Inspector(document.querySelector("#observablehq-viewof-colorBlendEffect-8955fadf"));
-  if (name === "svg") return new Inspector(document.querySelector("#observablehq-svg-8955fadf"));
-  return ["applyGradients","applyFilters","applyFilters1","applyColorBlends"].includes(name);
+  if (name === "viewof gradientEffect2") return new Inspector(document.querySelector("#observablehq-viewof-gradientEffect2-3b95f26a"));
+  if (name === "viewof filterEffect") return new Inspector(document.querySelector("#observablehq-viewof-filterEffect-3b95f26a"));
+  if (name === "viewof colorBlendEffect") return new Inspector(document.querySelector("#observablehq-viewof-colorBlendEffect-3b95f26a"));
+  if (name === "viewof colorBlendEffect2") return new Inspector(document.querySelector("#observablehq-viewof-colorBlendEffect2-3b95f26a"));
+  if (name === "svg") return new Inspector(document.querySelector("#observablehq-svg-3b95f26a"));
+  return ["applyGradients1","applyFilters","applyFilters1","applyColorBlends","applyColorBlends1"].includes(name);
 });
 

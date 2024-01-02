a33179f028858cecc4e31c0405a41e9659375436
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-01-02 03:41:04 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,81 @@
-// create a variable for the image
+let yoff = 0.0; // 2nd dimension of simplex noise
+let song, analyzer;
+let mic, fft;
+
 let Logo;
 // preload image
 function preload() {
-  Logo = loadImage("logo_lg.png");
-}
+  Logo = loadImage("logo_300.png");
+ 
+  font = loadFont('JosefinSans-Regular.ttf');
 
+}
 function setup() {
-  // create canvas the same size as the image
-  createCanvas(500,500);
+ 
+  createCanvas(1920, 1080);
+  frameRate(60);
+  background(0,0,0)
+  
+  
+ // fill('rgba(200,2,80, .003');
+  
 }
 
 function draw() {
+    
+loop();
+ fill('rgba200,200,200, 200.02)');
+ 
+
+
   
-  // show the 'initial image
-  Logo.loadPixels();
+ 
+  fill('rgba(00,00,0, .02)');
+  stroke(random(255),random(10),random(208),155.94);
+ strokeWeight(1.7)
+  beginShape();
+// Option #1: 4D Noise
+  let xoff = 0.0020; 
+  
+  
+   //let xoff = yoff; 
+   // Option #2: 1D Noise
+
+  // create a new Amplitude analyzer
+  analyzer = new p5.Amplitude();
+
+  // Patch the input to an volume analyzer
+  analyzer.setInput(song);
+  
+ 
+  
+    // Get the average (root mean square) amplitude
+  let rms = analyzer.getLevel();
+
+  
+
+
+
+  // Iterate over horizontal pixels
+  for (let x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to
+
+ 
+    // Option #1: 2D Noise
+    let y = map(noise(xoff, yoff), 0.0020,0.085, 900, 790);
+
+
+    // Set the vertex
+    vertex(x, y);
+    // Increment x dimension for noise
+    xoff += 0.035;
+  }
+  // increment y dimension for noise
+  yoff += 0.011;
+  vertex(width, height);
+  vertex(2, height);
+  endShape(CLOSE);
+   Logo.loadPixels();
   // go through each row
   for (let y = 0; y < height; y++) {
     // and each column
@@ -29,4 +91,7 @@ function draw() {
     Logo.updatePixels();
     // display manpulated image
   image(Logo, 0, 0);
+
+
+
 }
